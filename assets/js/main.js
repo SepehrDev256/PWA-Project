ea135676acf05f5e65a0c3f6f2cf37d41ce61760
SepehrDev256 <SBDeveloper8102@gmail.com>
2023-12-01 15:24:41 +0330

    restful API

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Sidenav.init(elems, {
         edge: "right"
     });
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+
+
 });
