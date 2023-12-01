a901d12f9320a5ddc041133815a6962794cc364d
SepehrDev256 <SBDeveloper8102@gmail.com>
2023-12-01 17:40:18 +0330

    add spiner

---
+++ assets/js/alpineJsContollers/usersController.js
@@ -0,0 +1,16 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('usersData', function(){
+        return {
+            users: [],
+            isLoading: false,
+            getUsers(){
+                this.isLoading = true
+                axios.get("https://jsonplaceholder.typicode.com/users").then((res)=>{
+                    this.users = res.data
+                    this.isLoading = false
+                    console.log(res);
+                })
+            }
+        }
+    })
+})
